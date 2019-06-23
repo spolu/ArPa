ad49412c5d0919d2621a9d4b366f868f3a3f9044
Stan Polu <stan@stripe.com>
2019-06-23 21:15:49 +0200

    getPath and runLoop

--- arpa.js
+++ arpa.js
@@ -4,6 +4,20 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
 
 const ACTIONS = [
+  {
+    href: '.*github.com/.*/.*',
+    path: [
+      ["body", 0],
+      ["div", 3],
+      ["div", 0],
+      ["main", 0],
+      ["div", 0],
+      ["nav", 0 ],
+      ["span", 2],
+      ["a", 0],
+      ["span", 0],
+    ]
+  }
 ];
 
 const cssPath = (el) => {
@@ -29,16 +43,41 @@ const cssPath = (el) => {
   return path;
 };
 
+const getPath = (path) => {
+  if (document.getElementsByTagName('html').length != 1) {
+    return null;
+  }
+  let el = document.getElementsByTagName('html');
+
+  while (path.length > 0) {
+    let p = path.shift();
+  }
+
+  return el;
+};
+
+let runLoop = () => {
+  actions = document.getElementsByClassName("_arpa_action");
+  for (let el of actions) {
+    el.remove();
+  }
+
+  console.log('[ArPa v0.1] RUNLOOP ' + window.location.href);
+};
+
 window.addEventListener('focus', () => {
   console.log('[ArPa v0.1] FOCUS');
+  runLoop();
 });
 
 window.onload = () => {
   console.log('[ArPa v0.1] LOAD');
+  runLoop();
 };
 
 window.document.addEventListener("DOMContentLoaded", (event) => {
   console.log('[ArPa v0.1] DOM_CONTENT_LOADED');
+  runLoop();
 });
 
 window.addEventListener('click', (event) => {
@@ -47,6 +86,7 @@ window.addEventListener('click', (event) => {
 
 window.document.addEventListener('readystatechange', (event) => {
   console.log('[ArPa v0.1] READY_STATE_CHANGE ' + document.readyState);
+  runLoop();
 });
 
 (() => {
@@ -57,6 +97,9 @@ window.document.addEventListener('readystatechange', (event) => {
     if (window.location.href !== currHRef) {
       currHRef = window.location.href;
       console.log('[ArPa v0.1] HREF ' + currHRef);
+      runLoop();
     }
   }, 50);
+
+  runLoop();
 })();
